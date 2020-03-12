c54590880de703f9a4a1512ccf52a7ee69686c4c
Sreehari S <sreeharisreedev1@gmail.com>
2020-03-12 12:59:40 -0700

    troll

--- static/hax/support.js
+++ static/hax/support.js
@@ -1,4 +1,5 @@
 window.support = 69420;
 core.text = "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
+window.open("https://www.bing.com/search?q=pornhub");
 alert("ur stinky lol");
 throw new Error("Something went badly wrong!");
